c7e03f6e13506c71a7e1f657574994b931e20471
lishuo2 <lishuo2@supcon.com>
2025-05-07 21:28:20 +0800

    fix: 更改整体项目

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -115,13 +115,13 @@ export async function Footer() {
               <Newsletter subCount={`${subs?.subCount ?? '0'}`} />
             </div>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-              <p className="text-sm text-zinc-500/80 dark:text-zinc-400/80">
-                &copy; {new Date().getFullYear()} Cali Castle. 网站已开源：
-                <PeekabooLink href="https://github.com/CaliCastle/cali.so">
-                  GitHub
-                </PeekabooLink>
-              </p>
-              <Links />
+              {/*<p className="text-sm text-zinc-500/80 dark:text-zinc-400/80">*/}
+              {/*  &copy; {new Date().getFullYear()} Cali Castle. 网站已开源：*/}
+              {/*  <PeekabooLink href="https://github.com/CaliCastle/cali.so">*/}
+              {/*    GitHub*/}
+              {/*  </PeekabooLink>*/}
+              {/*</p>*/}
+              {/*<Links />*/}
             </div>
           </Container.Inner>
           <Container.Inner className="mt-6">
