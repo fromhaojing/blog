7a2bb245f9bec59ac63afabb45c8685a52e782f3
fromhaojing <fromhaojing@gmail.com>
2025-08-12 23:40:16 +0800

    fix: 修改背景

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -14,12 +14,12 @@ export default function BlogLayout({
 }) {
   return (
     <>
-      <div className="pointer-events-none fixed inset-0 select-none bg-[url('/grid-black.svg')] bg-top bg-repeat dark:bg-[url('/grid.svg')]" />
+      <div className="pointer-events-none fixed inset-0 select-none bg-[url('/background.webp')] bg-center bg-cover  bg-no-repeat dark:bg-[url('/background.webp')]" />
       <span className="pointer-events-none fixed top-0 block h-[800px] w-full select-none bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(5,5,5,0.045)_0%,rgba(0,0,0,0)_100%)] dark:bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(255,255,255,0.09)_0%,rgba(255,255,255,0)_100%)]" />
 
-      <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-zinc-50/90 ring-1 ring-zinc-100 dark:bg-zinc-900/80 dark:ring-zinc-400/20" />
+      <div className="fixed inset-0 flex justify-center ">
+        <div className="flex w-full ">
+          <div className="w-full lg:px-8 sm:px-8 bg-zinc-50/50 ring-1 ring-zinc-100 backdrop-blur-lg dark:bg-zinc-900/70 dark:ring-zinc-400/20" />
         </div>
       </div>
 
@@ -27,7 +27,7 @@ export default function BlogLayout({
         <div className="relative text-zinc-800 dark:text-zinc-200">
           <Header />
           <main>{children}</main>
-          <Suspense>
+          <Suspense fallback={null}>
             <Footer />
           </Suspense>
         </div>
